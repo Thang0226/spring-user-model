e5810ae1ac844b0c008f7c94bfbe327c86db93fb
Thang0226 <thang.nd0226@gmail.com>
2024-12-18 16:20:52 +0700

    first commit

---
+++ src/main/java/com/codegym/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.codegym.controller;
+
+import com.codegym.DAO.UserDAO;
+import com.codegym.model.Login;
+import com.codegym.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+	@Autowired
+	private UserDAO userDAO;
+
+	@GetMapping("/home")
+	public ModelAndView home() {
+		ModelAndView modelAndView = new ModelAndView("home");
+		modelAndView.addObject("login", new Login());
+		return modelAndView;
+	}
+
+	@PostMapping("/login")
+	public ModelAndView login(@ModelAttribute("login") Login login){
+		User user = UserDAO.checkLogin(login);
+		ModelAndView modelAndView;
+		if(user == null){
+			modelAndView = new ModelAndView("error");
+		} else {
+			modelAndView = new ModelAndView("user");
+			modelAndView.addObject("user", user);
+		}
+		return modelAndView;
+	}
+}
